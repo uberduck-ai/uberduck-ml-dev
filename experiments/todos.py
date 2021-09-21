4d362b89485dfc4189982661f6bd8de870aec098
sjkoelle <s_uberduck_ai@uberduck-sam-t4-west2c.us-west2-c.c.uberduck.internal>
2021-09-21 22:44:18 +0000

    initial preprocessing experiments

--- experiments/todos.py
+++ experiments/todos.py
@@ -1,9 +1,22 @@
 from src.utils import get_dataset
+from src.models import TTSModel
+from src.models import TTSTrainer
 
-source_files = ['/mnt/disk/uberduck-experiments-v0/data/uberduck/eminem/all.txt','/mnt/disk/uberduck-experiments-v0/data/uberduck/LJSpeech-1.1/metadata.csv']
-uberduck/kanye-rap
-uberduck/alex-trebek
-uberduck/ben-shapiro
-uberduck/michael-rosen
-uberduck/steve-harvey
-uberduck/eminem/all.txt
+experiment_filelist_dir
+
+source_files = [experiment_filelist_dir + 'eminem_all.txt',
+            '/mnt/disk/uberduck-experiments-v0/data/uberduck/kanye-rap/all.txt',
+            '/mnt/disk/uberduck-experiments-v0/data/uberduck/alex-trebek/all.txt',
+            '/mnt/disk/uberduck-experiments-v0/data/uberduck/ben-shapiro/all.txt',
+            '/mnt/disk/uberduck-experiments-v0/data/uberduck/michael-rosen/all.txt',
+            '/mnt/disk/uberduck-experiments-v0/data/uberduck/steve-harvey/all.txt',
+            '/mnt/disk/uberduck-experiments-v0/data/uberduck/LJSpeech-1.1/metadata.csv',
+            '/mnt/disk/uberduck-experiments-v0/data/uberduck/LibriTTS/metadata_mellotron.txt',
+            '/mnt/disk/uberduck-experiments-v0/data/uberduck/vctk/metadata.txt']
+
+synthesize_speakerids(source_files) #need to change
+reindex()
+
+train,val  = get_testval_split()
+
+TTSModel('mellotron')
