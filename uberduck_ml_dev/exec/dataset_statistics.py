7ae5f21424174c7a5c2042e45fafe2d6b89e8cd0
Will Luer <will.luer@gmail.com>
2021-10-10 15:53:34 +0000

    uberduck g2p build

--- uberduck_ml_dev/exec/dataset_statistics.py
+++ uberduck_ml_dev/exec/dataset_statistics.py
@@ -150,7 +150,12 @@ def calculate_statistics(
 
                 # Transcription
                 word_freqs.extend(word_frequencies(transcription_cleaned))
-                transcription_lookups = g2p.check_lookup(transcription_cleaned)
+                try:
+                    transcription_lookups = g2p.check_lookup(transcription_cleaned)
+                except:
+                    print(
+                        "[INFO] Install the Uberduck forked g2p to obtain words not found in CMU dict: https://github.com/uberduck-ai/g2p"
+                    )
                 for k in transcription_lookups:
                     lookup_results[k].extend(transcription_lookups[k])
 
