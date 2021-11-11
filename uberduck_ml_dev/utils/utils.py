19caec878ceedf5f6c5b8b36fee7cc9e53f8c4f4
zwf <zwf@uberduck>
2021-11-10 17:06:23 -0800

    Add utility code and nn modules

--- uberduck_ml_dev/utils/utils.py
+++ uberduck_ml_dev/utils/utils.py
@@ -3,7 +3,8 @@
 __all__ = ['load_filepaths_and_text', 'synthesize_speakerids2', 'parse_vctk', 'parse_libritts_mellotron',
            'add_speakerid', 'parse_libritts_mellotron', 'parse_uberduck', 'parse_ljspeech', 'get_alignment_metrics',
            'window_sumsquare', 'griffin_lim', 'dynamic_range_compression', 'dynamic_range_decompression', 'to_gpu',
-           'get_mask_from_lengths', 'reduce_tensor']
+           'get_mask_from_lengths', 'reduce_tensor', 'subsequent_mask', 'convert_pad_shape', 'sequence_mask',
+           'generate_path', 'rand_slice_segments', 'init_weights', 'get_padding', 'fused_add_tanh_sigmoid_multiply']
 
 # Cell
 
@@ -338,3 +339,72 @@ def reduce_tensor(tensor, n_gpus):
     dist.all_reduce(rt, op=dist.ReduceOp.SUM)
     rt /= n_gpus
     return rt
+
+# Cell
+def subsequent_mask(length):
+    mask = torch.tril(torch.ones(length, length)).unsqueeze(0).unsqueeze(0)
+    return mask
+
+# Cell
+def convert_pad_shape(pad_shape):
+    """Reverse, then flatten a list of lists."""
+    l = pad_shape[::-1]
+    pad_shape = [item for sublist in l for item in sublist]
+    return pad_shape
+
+# Cell
+def sequence_mask(length, max_length=None):
+    """The same as get_mask_from_lengths"""
+    if max_length is None:
+        max_length = length.max()
+    x = torch.arange(max_length, dtype=length.dtype, device=length.device)
+    return x.unsqueeze(0) < length.unsqueeze(1)
+
+# Cell
+def generate_path(duration, mask):
+    """
+    duration: [b, 1, t_x]
+    mask: [b, 1, t_y, t_x]
+    """
+    device = duration.device
+
+    b, _, t_y, t_x = mask.shape
+    cum_duration = torch.cumsum(duration, -1)
+
+    cum_duration_flat = cum_duration.view(b * t_x)
+    path = sequence_mask(cum_duration_flat, t_y).to(mask.dtype)
+    path = path.view(b, t_x, t_y)
+    path = path - F.pad(path, convert_pad_shape([[0, 0], [1, 0], [0, 0]]))[:, :-1]
+    path = path.unsqueeze(1).transpose(2, 3) * mask
+    return path
+
+# Cell
+def rand_slice_segments(x, x_lengths=None, segment_size=4):
+    b, d, t = x.size()
+    if x_lengths is None:
+        x_lengths = t
+    ids_str_max = x_lengths - segment_size + 1
+    ids_str = (torch.rand([b]).to(device=x.device) * ids_str_max).to(dtype=torch.long)
+    ret = slice_segments(x, ids_str, segment_size)
+    return ret, ids_str
+
+# Cell
+def init_weights(m, mean=0.0, std=0.01):
+    classname = m.__class__.__name__
+    if classname.find("Conv") != -1:
+        m.weight.data.normal_(mean, std)
+
+
+# Cell
+def get_padding(kernel_size, dilation=1):
+    return int((kernel_size * dilation - dilation) / 2)
+
+# Cell
+@torch.jit.script
+def fused_add_tanh_sigmoid_multiply(input_a, input_b, n_channels):
+    n_channels_int = n_channels[0]
+    in_act = input_a + input_b
+    t_act = torch.tanh(in_act[:, :n_channels_int, :])
+    s_act = torch.sigmoid(in_act[:, n_channels_int:, :])
+    acts = t_act * s_act
+    return acts
