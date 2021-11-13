521af6abd43245261f919d7af0139ec532e0e850
zwf <zwf@uberduck>
2021-11-12 16:50:56 -0800

    revert bad change and fix off by one

--- uberduck_ml_dev/utils/utils.py
+++ uberduck_ml_dev/utils/utils.py
@@ -396,7 +396,7 @@ def rand_slice_segments(x, x_lengths=None, segment_size=4):
     b, d, t = x.size()
     if x_lengths is None:
         x_lengths = t
-    ids_str_max = x_lengths - segment_size + 1
+    ids_str_max = x_lengths - segment_size
     ids_str = (torch.rand([b]).to(device=x.device) * ids_str_max).to(dtype=torch.long)
     ret = slice_segments(x, ids_str, segment_size)
     return ret, ids_str
