119beb0cb68e17d150052a1e60f751f2de43fbbe
sjkoelle <s_uberduck_ai@uberduck-sam-t4-west2c.us-west2-c.c.uberduck.internal>
2021-12-14 20:38:58 +0000

    some docker stuff no secrets

---
+++ docker/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+gcloud auth activate-service-account --key-file=/secrets/gcloud_key.json
+export GOOGLE_APPLICATION_CREDENTIALS=/secrets/gcloud_key.json
+export BUCKET=$1
+export UBMLEXP=$2
+export WSN=$3
+export FILELIST_TRAIN=$4
+export FILELIST_VAL=$5
+export NAME=$6
+export RESULT_DIR = ${BUCKET}/results
+export DATA_DIR = ${BUCKET}/data
+export MODEL_DIR = ${BUCKET}/models
+export LOG_DIR = ${RESULT_DIR}/${NAME}/logs
+export CHECKPOINT_PATH = ${RESULT_DIR}/${NAME}/checkpoints
+export WARM_START_NAME = ${MODEL_DIR}/${WSN}
+
+git clone -b sam-exp $UBMLEXP
+gcsfuse $BUCKET /bucket
+python -m uberduck_ml_dev.exec.train_tacotron2 --config ./uberduck-ml-exp/experiments/taco2_lj_lachow/config.json --log_dir $LOG_DIR --checkpoint_path $CHECKPOINT_PATH --training_audiopaths_and_text $FILELIST_TRAIN --val_audiopaths_and_text $FILELIST_VAL --warm_start_name $WARM_START_NAME
