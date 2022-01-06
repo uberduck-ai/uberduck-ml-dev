d79d20d79ad9434fef74ad341c68427fba04ad55
sjkoelle <s_uberduck_ai@uberduck-sam-t4-west2c.us-west2-c.c.uberduck.internal>
2022-01-06 20:57:17 +0000

    rest of streamlit files and moved diagonalness

---
+++ uberduck_ml_dev/None.py
@@ -0,0 +1,15 @@
+
+
+# Cell
+
+import streamlit as st
+
+# Cell
+
+import streamlit as st
+from collections import OrderedDict
+from .monitoring.generate import (
+    generate_inferences,
+    MODEL_LIST,
+    MODEL_TYPES,
+)
