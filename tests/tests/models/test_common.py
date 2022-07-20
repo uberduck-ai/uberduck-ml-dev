11f9f20deb5e7f24c4db181c75a0f249148aaec9
Samson Koelle <samsonkoelle@Samsons-MacBook-Pro-2.local>
2022-07-19 22:42:38 -0700

    more tests

---
+++ tests/tests/models/test_common.py
@@ -0,0 +1,10 @@
+from uberduck_ml_dev.models.common import MelSTFT
+import torch
+
+
+class TestCommon:
+    def test_mel_stft():
+        mel_stft = MelSTFT()
+        mel = mel_stft.mel_spectrogram(torch.clip(torch.randn(1, 1000), -1, 1))
+        assert mel.shape[0] == 1
+        assert mel.shape[1] == 80
---
+++ tests/tests/text/test_symbols.py
@@ -0,0 +1,20 @@
+from uberduck_ml_dev.text.symbols import arpabet_to_sequence, symbols_to_sequence
+
+
+class TestSymbols:
+    def test_arpabet_to_sequence(self):
+
+        # NOTE: arpabet_to_sequence does not properly handle whitespace, it should take single words only.
+        assert (
+            len(
+                arpabet_to_sequence(
+                    "{ S IY } { EH M } { Y UW } { D IH K SH AH N EH R IY }"
+                )
+            )
+            == 15
+        )
+        assert arpabet_to_sequence("{ S IY }") == [168, 148]
+        # But symbols_to_sequence hanldes whitespace
+
+    def test_symbols_to_sequence(self):
+        assert len(symbols_to_sequence("C M U Dictionary")) == 16
