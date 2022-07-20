99ab6a3e12cf86845c0cedcd4efb4bb83e3cfed1
Samson Koelle <samsonkoelle@Samsons-MacBook-Pro-2.local>
2022-07-19 17:31:17 -0700

    hifigan tests;

---
+++ tests/tests/vocoders/test_hifi_gan.py
@@ -0,0 +1,22 @@
+from scipy.io.wavfile import read
+from uberduck_ml_dev.models.common import MelSTFT
+import torch
+
+
+class TestHifiGan:
+    def test_hifi_gan(self):
+
+        path = "test/fixtures/wavs/stevejobs-1.wav"
+        sr, data = read(path)
+
+        assert sr == 22050
+        assert len(data) == 144649
+
+        data = torch.FloatTensor(data / 32768.0).unsqueeze(0)
+
+        melstft = MelSTFT()
+        mel = melstft.mel_spectrogram(data)
+
+        assert mel.shape[0] == 1
+        assert mel.shape[1] == 80
+        assert mel.shape[2] == 566
