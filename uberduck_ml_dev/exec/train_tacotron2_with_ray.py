741b0cf8f433edfbf922a5922a09dbad44a0b306
ZWF <z@uberduck.ai>
2023-02-14 08:40:42 -0800

    updates

--- uberduck_ml_dev/exec/train_tacotron2_with_ray.py
+++ uberduck_ml_dev/exec/train_tacotron2_with_ray.py
@@ -16,17 +16,19 @@ import ray.train as train
 from ray.train.torch import TorchTrainer
 
 
-# from ..trainer.tacotron2 import Tacotron2Trainer, DEFAULTS as TACOTRON2_TRAINER_DEFAULTS
 from uberduck_ml_dev.losses import Tacotron2Loss
-from uberduck_ml_dev.models.tacotron2 import Tacotron2, DEFAULTS
+from uberduck_ml_dev.models.tacotron2 import Tacotron2, DEFAULTS, INFERENCE
 from uberduck_ml_dev.data.collate import Collate
 from uberduck_ml_dev.data.batch import Batch
 from uberduck_ml_dev.text.utils import text_to_sequence
 from uberduck_ml_dev.text.symbols import NVIDIA_TACO2_SYMBOLS
+from uberduck_ml_dev.trainer.tacotron2 import Tacotron2Trainer
+from uberduck_ml_dev.trainer.base import sample
 from uberduck_ml_dev.models.common import MelSTFT
-# from uberduck_ml_dev.utils.plot import save_figure_to_numpy
+from uberduck_ml_dev.utils.plot import save_figure_to_numpy, plot_spectrogram, plot_gate_outputs, plot_attention
 # from uberduck_ml_dev.utils.utils import reduce_tensor
 from uberduck_ml_dev.monitoring.statistics import get_alignment_metrics
+from uberduck_ml_dev.text.utils import random_utterance
 
 config = DEFAULTS.values()
 config["with_gsts"] = False
@@ -41,6 +43,47 @@ stft = MelSTFT(
     mel_fmax=DEFAULTS.mel_fmax,
     padding=None,
 )
+
+def sample_inference(model):
+    with torch.no_grad():
+        transcription = random_utterance()
+        utterance = torch.LongTensor(
+            text_to_sequence(
+                transcription,
+                ["english_cleaners"],
+                p_arpabet=1.0,
+                symbol_set=NVIDIA_TACO2_SYMBOLS,
+            )
+        )[None]
+
+        input_lengths = torch.LongTensor([utterance.shape[1]])
+        speaker_id_tensor = None # torch.LongTensor([speaker_id])
+
+        if torch.cuda.is_available():
+            utterance = utterance.cuda()
+            input_lengths = input_lengths.cuda()
+            gst_embedding = None
+            speaker_id_tensor = None # speaker_id_tensor.cuda()
+
+        model.eval()
+        model_output = model.forward(
+            input_text=utterance,
+            input_lengths=input_lengths,
+            speaker_ids=speaker_id_tensor,
+            # NOTE (Sam): this is None if using old multispeaker training, not None if using new pretrained encoder.
+            audio_encoding=None, #speaker_embedding,
+            embedded_gst=None, # gst_embedding,
+            mode=INFERENCE,
+        )
+        model.train()
+        audio = sample(model_output["mel_outputs_postnet"][0])
+        if (audio.size(0) == 1 or audio.size(0) == 2) and audio.size(1) > 2:
+            audio = audio.transpose(0, 1)
+        return audio
+
+
+
+
 
 def ray_df_to_batch(df):
     transcripts = df.transcript.tolist()
@@ -119,31 +162,62 @@ def log(
         "MelLoss/train": mel_loss.item(),
         "GateLoss/train": gate_loss.item(),
         "GradNorm": grad_norm.item(),
-        epoch: epoch,
+        "epoch": epoch,
     }
     batch_levels = X["speaker_ids"]
-    batch_levels_unique = torch.unique(batch_levels)
+    batch_levels_unique = torch.unique(batch_levels) if batch_levels else []
     for l in batch_levels_unique:
         mlb = mel_loss_batch[torch.where(batch_levels == l)[0]].mean()
         metrics[f"MelLoss/train/speaker{l.item()}"] = mlb
         glb = gate_loss_batch[torch.where(batch_levels == l)[0]].mean()
         metrics[f"GateLoss/train/speaker{l.item()}"] = glb
         metrics[f"Loss/train/speaker{l.item()}"] = mlb + glb
-    if global_step % steps_per_sample:
+    session.report(metrics)
+    if global_step % steps_per_sample == 0:
         alignment_metrics = get_alignment_metrics(y_pred["alignments"])
         alignment_diagonalness = alignment_metrics["diagonalness"]
-        wandb.log({
-            "AlignmentDiagonalness/train": wandb.Image(alignment_diagonalness)
-
-        })
+        alignment_max = alignment_metrics["max"]
+        if session.get_world_rank() == 0 or session.get_world_size() == 1:
+            teacher_forced = sample(y_pred["mel_outputs_postnet"][0]).transpose(0, 1)
+            sample_audio = sample_inference(model)
+            wandb_metrics = dict(metrics)
+            input_length = X["input_lengths"][0].item()
+            output_length = X["output_lengths"][0].item()
+            mel_target = y["mel_padded"][0]
+            wandb_metrics.update({
+                "AlignmentDiagonalness/train": alignment_diagonalness.item(),
+                "AlignmentMax/train": alignment_max.item(),
+                "MelPredicted/train": wandb.Image(save_figure_to_numpy(plot_spectrogram(y_pred["mel_outputs_postnet"][0].data.cpu()))),
+                "MelTarget/train": wandb.Image(save_figure_to_numpy(plot_spectrogram(mel_target.data.cpu()))),
+                "TargetAudio/train": wandb.Audio(sample(mel_target).transpose(0, 1), sample_rate=22050),
+                "Gate/train": wandb.Image(
+                    save_figure_to_numpy(plot_gate_outputs(
+                        gate_targets=y["gate_target"][0].data.cpu(),
+                        gate_outputs=y_pred["gate_predicted"][0].data.cpu(),
+                    ))
+                ),
+                "Attention/train": wandb.Image(
+                    save_figure_to_numpy(
+                        plot_attention(
+                            y_pred["alignments"][0].data.cpu().transpose(0, 1),
+                            encoder_length=input_length,
+                            decoder_length=output_length,
+                        )
+                    )
+                ),
+                "SampleInference": wandb.Audio(sample_audio, sample_rate=22050),
+                "AudioTeacherForced/train": wandb.Audio(teacher_forced, sample_rate=22050),
+            })
+            wandb.log(wandb_metrics)
 
 
 
 def train_func(config: dict):
-    setup_wandb(config)
+    setup_wandb(config, project="voice-cloning")
     print("CUDA AVAILABLE: ", torch.cuda.is_available())
     is_cuda = torch.cuda.is_available()
     DEFAULTS.cudnn_enabled = is_cuda
+    DEFAULTS.steps_per_sample = 5
     device = train.torch.get_device()
     DEFAULTS.device = device
     torch.cuda.set_device(device)
@@ -205,19 +279,12 @@ def train_func(config: dict):
                 global_step,
                 DEFAULTS.steps_per_sample,
             )
-            # session.report({
-            #     "Loss/train": loss.item(),
-            #     "MelLoss/train": mel_loss.item(),
-            #     "GateLoss/train": gate_loss.item(),
-            #     "GradNorm": grad_norm.item(),
-            #     epoch: epoch,
-            # })
             optimizer.step()
 
         session.report(
             {},
             checkpoint=Checkpoint.from_dict(
-                dict(epoch=epoch, model=model.state_dict())
+                dict(epoch=epoch, global_step=global_step, model=model.state_dict())
             )
         )
 
@@ -229,16 +296,7 @@ if __name__ == "__main__":
     trainer = TorchTrainer(
         train_loop_per_worker=train_func,
         train_loop_config={"lr": 1e-3, "batch_size": 5, "epochs": 100},
-        scaling_config=ScalingConfig(num_workers=4, use_gpu=True, resources_per_worker=dict(CPU=4, GPU=1)),
-        run_config=RunConfig(
-            callbacks=[
-                WandbLoggerCallback(
-                    project="voice-cloning",
-                    save_checkpoints=True,
-                )
-            ]
-
-        ),
+        scaling_config=ScalingConfig(num_workers=1, use_gpu=True, resources_per_worker=dict(CPU=4, GPU=1)),
         datasets={"train": ray_dataset},
     )
     result = trainer.fit()
