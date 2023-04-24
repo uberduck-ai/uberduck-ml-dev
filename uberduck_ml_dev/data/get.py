101247b5f865c101a24463af1c4c3f8824e46b8b
Samson Koelle <sjkoelle@gmail.com>
2023-04-24 11:19:28 -0700

    fixed

--- uberduck_ml_dev/data/get.py
+++ uberduck_ml_dev/data/get.py
@@ -4,6 +4,9 @@ from uberduck_ml_dev.data.data import DataMel, DataPitch, DataEmbedding
 from uberduck_ml_dev.data.collate import CollateBlank
 
 
+# NOTE (Sam): assumes data is in a directory structure like:
+# /tmp/{uuid}/resampled_normalized.wav
+# These functions add spectrogram.pt, f0.pt, and coqui_resnet_512_emb.pt to each file-specific directory.
 def get_mels(paths, data_config):
     data = DataMel(audiopaths=paths, data_config=data_config)
 
@@ -18,11 +21,18 @@ def get_mels(paths, data_config):
         pass  # computes in loader.
 
 
-def get_embeddings(paths, data_config, resnet_se_model_path, resnet_se_config_path):
+def get_embeddings(
+    paths,
+    data_config,
+    resnet_se_model_path,
+    resnet_se_config_path,
+    subpath_truncation=41,
+):
     data = DataEmbedding(
         audiopaths=paths,
         resnet_se_model_path=resnet_se_model_path,
         resnet_se_config_path=resnet_se_config_path,
+        subpath_truncation=subpath_truncation,
     )
 
     collate_fn = CollateBlank()
@@ -36,8 +46,10 @@ def get_embeddings(paths, data_config, resnet_se_model_path, resnet_se_config_pa
         pass  # computes in loader.
 
 
-def get_pitches(paths, data_config):
-    data = DataPitch(audiopaths=paths, data_config=data_config)
+def get_pitches(paths, data_config, subpath_truncation=41):
+    data = DataPitch(
+        audiopaths=paths, data_config=data_config, subpath_truncation=subpath_truncation
+    )
 
     collate_fn = CollateBlank()
 
