396fbc32b1c72e2f593f0c230fc7b8d30733b8f2
Samson Koelle <sjkoelle@gmail.com>
2023-05-25 10:25:14 -0700

    rename

--- uberduck_ml_dev/data/get.py
+++ uberduck_ml_dev/data/get.py
@@ -3,14 +3,6 @@ from torch.utils.data import DataLoader
 from uberduck_ml_dev.data.data import DataMel, DataPitch, DataEmbedding
 from uberduck_ml_dev.data.collate import CollateBlank
 
-# NOTE (Sam): this is a horrible name for a function thats borrowed from a bad name from rvc and yet somehow now even worse
-# since it gets "pitch" and "pitchf" (which are terms we should remove since they are unclear).
-# TODO (Sam): rename this to get_pitches
-def get_pitchesf(paths, data_config = None, subpath_truncation=41, method = 'parselmouth', sample_rate = 16000):
-    data = DataPitch(
-        audiopaths=paths, data_config=data_config, subpath_truncation=subpath_truncation, method = method, sample_rate = sample_rate
-    )
-    get_parallel_torch(data)
 
 def get_parallel_torch(data):
 
@@ -19,7 +11,7 @@ def get_parallel_torch(data):
         pass
 
 
-# TODO (Sam): replace with get_parallel_torch
+# TODO (Sam): use get_parallel_torch to reduce boilerplate.
 # NOTE (Sam): assumes data is in a directory structure like:
 # /tmp/{uuid}/resampled_normalized.wav
 # These functions add spectrogram.pt, f0.pt, and coqui_resnet_512_emb.pt to each file-specific directory.
@@ -61,21 +53,10 @@ def get_embeddings(
     for batch in data_loader:
         pass  # computes in loader.
 
-# NOTE (Sam): pitch_method isn't inherently inflexible, but reflects the reality of training.
-def get_pitches(paths, data_config, subpath_truncation=41):
 
-
+# NOTE (Sam): pitch, pitchf == f0 coarse, f0bak in rvc parlance.
+def get_pitches(paths, data_config = None, subpath_truncation=41, method = 'parselmouth', sample_rate = 16000):
     data = DataPitch(
-        audiopaths=paths, data_config=data_config, subpath_truncation=subpath_truncation
+        audiopaths=paths, data_config=data_config, subpath_truncation=subpath_truncation, method = method, sample_rate = sample_rate
     )
-
-    collate_fn = CollateBlank()
-
-    data_loader = DataLoader(
-        data,
-        batch_size=32,
-        collate_fn=collate_fn,
-    )
-    for batch in data_loader:
-        pass  # computes in loader.
-
+    get_parallel_torch(data)
