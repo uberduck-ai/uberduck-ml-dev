4e64cac6240fbd689a3c53c215619b439db6c293
sjkoelle@gmail.com <sjkoelle@gmail.com>
2023-07-20 17:03:49 -0700

    save

--- uberduck_ml_dev/data/get.py
+++ uberduck_ml_dev/data/get.py
@@ -11,7 +11,7 @@ from uberduck_ml_dev.data.collate import CollateBlank
 
 def get_parallel_torch(data):
     data_loader = DataLoader(
-        data, batch_size=32, collate_fn=CollateBlank(), num_workers=8
+        data, batch_size=32, collate_fn=CollateBlank(), num_workers=20
     )
     for batch in data_loader:
         pass
@@ -114,3 +114,25 @@ def get_hubert_embeddings(
         hubert_abs_paths.append(hubert_abs_path)
 
     return hubert_abs_paths
+
+
+from uberduck_ml_dev.data.data import FunctionalDataProcessor
+
+
+def get(
+    function_,
+    loading_function,
+    saving_function,
+    paths,
+    target_paths,
+    recompute,
+):
+    data = FunctionalDataProcessor(
+        paths=paths,
+        target_paths=target_paths,
+        function_=function_,
+        loading_function=loading_function,
+        saving_function=saving_function,
+        recompute=recompute,
+    )
+    get_parallel_torch(data)
