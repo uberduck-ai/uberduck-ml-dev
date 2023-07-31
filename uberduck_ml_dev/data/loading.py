0f4be344616eb12fd43037d28747f84b42bad740
sjkoelle@gmail.com <sjkoelle@gmail.com>
2023-07-31 16:09:48 -0700

    delete

--- uberduck_ml_dev/data/loading.py
+++
@@ -1,9 +0,0 @@
-from scipy.io.wavfile import read
-import torch
-from einops import rearrange
-
-
-def load_audio(path):
-    rate, data = read(path)
-    data = torch.Tensor(data)
-    return rearrange(data, "t -> 1 t")
