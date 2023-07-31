6ae343206284d5faa3c54c38ca7a28bf679e11ec
sjkoelle@gmail.com <sjkoelle@gmail.com>
2023-07-31 15:52:04 -0700

    save

--- tutorials/hifigan/download.sh
+++ tutorials/hifigan/download.sh
@@ -2,5 +2,5 @@
 
 wget https://huggingface.co/datasets/Uberduck/ljspeech/resolve/main/lj_speech.zip
 unzip lj_speech.zip
-wget https://huggingface.co/datasets/Uberduck/ljspeech/resolve/main/hifigan_libritts100360_generator0p5.pt
-wget https://huggingface.co/datasets/Uberduck/ljspeech/resolve/main/hifigan_22khz_config.json
+# wget https://huggingface.co/datasets/Uberduck/ljspeech/resolve/main/hifigan_libritts100360_generator0p5.pt
+# wget https://huggingface.co/datasets/Uberduck/ljspeech/resolve/main/hifigan_22khz_config.json
