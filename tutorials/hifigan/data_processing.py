703f578328bfd9a2f716acbe0af8db68bb422d1a
sjkoelle@gmail.com <sjkoelle@gmail.com>
2023-07-31 15:49:50 -0700

    preprocess

--- tutorials/hifigan/data_processing.py
+++ tutorials/hifigan/data_processing.py
@@ -10,7 +10,7 @@ from uberduck_ml_dev.data.data import HIFIGAN_DEFAULTS as DEFAULTS
 from uberduck_ml_dev.data.data import MAX_WAV_VALUE
 
 
-data_directory = "/usr/src/app/uberduck_ml_exp/data/lj_redo/"
+data_directory = ""  # path to the directory containing the data
 ground_truth_rel_paths = find_rel_paths(directory=data_directory, filename="gt.wav")
 ground_truth_abs_paths = [
     os.path.join(data_directory, ground_truth_rel_path)
@@ -18,7 +18,7 @@ ground_truth_abs_paths = [
 ]
 
 
-print("resampling and integer/32768 normalizing spectrograms")
+print("resampling and integer normalizing")
 
 resampled_normalized_abs_paths = [
     resampled_normalized_abs_path.replace(
@@ -45,8 +45,33 @@ get(
     True,
 )
 
+print("resampling and float normalizing")
 
-print("compute spectrograms")
+resampled_normalized_abs_paths = [
+    resampled_normalized_abs_path.replace("gt.wav", "audio_resampledT_normalized1T.wav")
+    for resampled_normalized_abs_path in ground_truth_abs_paths
+]
+
+loading_function = lambda filename: librosa.load(filename, sr=22050)[0]
+processing_function = lambda x: np.asarray(
+    (x / np.abs(x).max()) * (1 - 1 / MAX_WAV_VALUE), dtype=np.float32
+)
+saving_function = lambda data, filename: write(
+    filename, 22050, data
+)  # must be in this order
+
+
+get(
+    processing_function,
+    saving_function,
+    loading_function,
+    ground_truth_abs_paths,
+    resampled_normalized_abs_paths,
+    True,
+)
+
+
+print("computing spectrograms from 1 normalized audio")
 
 spectrogram_abs_paths = [
     ground_truth_abs_path.replace("gt.wav", "spectrogram.pt")
